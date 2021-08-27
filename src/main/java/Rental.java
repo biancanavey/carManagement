b23d8f872b014cd58681cf76d931332497dbb5dc
biancanavey <74916027+biancanavey@users.noreply.github.com>
2021-08-27 12:06:24 +0100

    first commit

---
+++ src/main/java/Rental.java
@@ -0,0 +1,23 @@
+public class Rental {
+}
+/        RENTAL
+//Should have a list as propert
+//Methods
+//add cars
+//remove cars
+
+//        - The user should be able to book a car
+// use Enum - Rental status - available/unavailable - update status of car
+//        - The user should be able to return a car
+// use a loop to search database - same plate number match
+// if/conditional change the status
+
+//        4 Things to take into consideration:
+//        Create a class called CarRentalDB:
+//        - You should have one property that can store a list of cars with corresponding details
+//  use a constructor - Brand(Enum),Price,Colour
+// use a string for registration - Use as ID
+//        - Create a public method for adding a car to the list
+//        - Create a public method for deleting a car from the the list if car exists
+//        - think about other methods that your application needs
+//
---
+++ src/main/java/carManagement.java
@@ -0,0 +1,23 @@
+public class carManagement {
+}
+//        CAR MANAGEMENT
+//        - The user should be able to add new cars including daily price for renting
+// Scanner
+// myCar.add - Arraylist
+//  Daily price use Bigdecimal
+//  string for car registration
+
+
+
+//        - The user should be able to remove cars if not being rented
+ //.remove
+
+
+//        - The user should be able to list available cars for rent
+    //arraylist - availible cars - if statement - sout
+
+
+
+//        - The user should be able to list rented cars
+//  arraylist - unavailible cars - if statement - sout
+
